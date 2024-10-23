02a296e02b79b9e31d5dec4a640ccc37f1d2bd63
cassandraysabel <cassandraysabel.gallo-21@cpu.edu.ph>
2024-10-23 23:37:14 +0800

    pls fix my code i will cri

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,25 @@
+const inputBox = document.getElementById('enter-task') as HTMLInputElement;
+const listContainer = document.getElementById('list-container');
 
-import "./style.css"
-
-let taskList = []
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const inputBox = document.getElementById('input-box') as HTMLInputElement;
-    
-    document.getElementById("add")?.addEventListener(("click"), ()=> {
-        
-        console.log("h")
-        
-        const task = document.createTextNode(inputBox.value)
-        console.log(task)
-        const spanList = document.createElement("span")
-        const div = document.createElement("div")
-        div.appendChild(task)
-        spanList.appendChild(div)
-    })
-});
+function addTask() {
+    if (!inputBox || !listContainer) return;
+    if (inputBox.value === '') {
+        alert('Please enter a task');
+    } else {
+        let li = document.createElement("li");
+        li.textContent = inputBox.value; 
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.textContent = "x";  
+        li.appendChild(span);
+    }
+    inputBox.value = '';  
+}
+listContainer?.addEventListener("click", function(e) {
+    const target = e.target as HTMLElement;
+    if (target.tagName === "LI") {
+        target.classList.toggle("done");
+    } else if (target.tagName === "SPAN") {
+        target.parentElement?.remove();
+    }
+}, false);
