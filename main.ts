7341f3454a9f1079a6619f9b3af1962b605ef6be
Huenyeon <alhenajulienne.gedalanga-21@cpu.edu.ph>
2024-10-23 20:29:43 +0800

    add button n functionality

--- main.ts
+++ main.ts
@@ -1,5 +1,20 @@
+let taskList = []
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const inputBox = document.getElementById('input-box') as HTMLInputElement;
+    const addButton = document.getElementById("add")?.addEventListener(("click"), ()=> {
+        const task  = document.createTextNode(inputBox.value)
+        console.log(task)
+        const spanList = document.createElement("span")
+        const div = document.createElement("div")
+
+        div.appendChild(task)
+        spanList.appendChild(div)
+
+        
+
+    })
     
+
 });
