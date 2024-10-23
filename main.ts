7bacb29f53a1de02ef699af3851c934b2e42fe72
Huenyeon <alhenajulienne.gedalanga-21@cpu.edu.ph>
2024-10-23 21:15:31 +0800

    wala na const

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ let taskList = []
 
 document.addEventListener('DOMContentLoaded', () => {
     const inputBox = document.getElementById('input-box') as HTMLInputElement;
-    const addButton = document.getElementById("add")?.addEventListener(("click"), ()=> {
+    document.getElementById("add")?.addEventListener(("click"), ()=> {
         const task  = document.createTextNode(inputBox.value)
         console.log(task)
         const spanList = document.createElement("span")
